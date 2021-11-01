23a2d35fe061452bd8c2903c92ba452811142b95
Juan Daniel <juan@mctekk.com>
2021-11-01 19:09:18 -0400

    fix demo

--- main.js
+++ main.js
@@ -5,16 +5,32 @@ const barcodeFormats = ["CODE_39", "CODE_128"];
 
 const config = {
     containerId: "scanner",
+    style: {
+        window: {
+            aspectRatio: 1,
+            paddingPropLeft: 0.1,
+            borderColor: "white",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+        },
+        hint: "",
+    },
     onBarcodesDetected: onBarcodesDetected,
     onError: onScannerError,
     barcodeFormats: barcodeFormats,
 };
 
 async function onBarcodesDetected(e) {
-    window.alert(e);
+    let text = "";
+    e.barcodes.forEach((barcode) => {
+        text += " " + barcode.text + " (" + barcode.format + "),";
+    });
+    window.alert(`code ${text.slice(0, -1)}`);
 }
 
 async function onScannerError(e) {
+    window.alert(`Error Scanning: ${e}`);
     console.log("Error:", e);
 }
 
@@ -26,6 +42,7 @@ async function onScannerError(e) {
         });
         await scanbotSDK.createBarcodeScanner(config);
     } catch (e) {
+        window.alert(`Error: ${e}`);
         console.log(e);
     }
 })();
